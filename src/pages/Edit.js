18d493a6d8af00ec63dc63fa0609685f038559ae
Anise-002 <n-u-002@naver.com>
2022-06-25 03:09:58 +0900

    TIL-React Router dom : useSearchParams, useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,30 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+
 const Edit = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const id = searchParams.get('id');
+    const mode = searchParams.get('mode');
+
+    console.log("id :", id, "/ mode : ", mode);
+
+
     return (
         <div>
             <h1>Edit</h1>
             <p>이 곳은 수정페이지 입니다.</p>
+            <button onClick={() => setSearchParams({ who: "hhhh" })}>Query String 바꾸기</button>
+
+            <button onClick={() => {
+                navigate('/home');
+            }}>HOME으로 가기</button>
+            <button onClick={() => {
+                navigate(-1);
+            }}>뒤로가기</button>
         </div>
+
     );
 };
 
